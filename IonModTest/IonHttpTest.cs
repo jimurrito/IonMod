370691d9f59ce6076b8ab44695bac587846a8ab4
jimurrito <jimurrito@gmail.com>
2024-06-01 13:24:48 -0700

    Added test module

---
+++ IonModTest/IonHttpTest.cs
@@ -0,0 +1,12 @@
+// https://learn.microsoft.com/en-us/visualstudio/test/using-microsoft-visualstudio-testtools-unittesting-members-in-unit-tests?view=vs-2022
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IonModTest {
+
+    [TestClass]
+    public class IonHttpTest {
+        
+
+    }
+
+}
---
+++ IonModTest/Program.cs
@@ -0,0 +1,4 @@
+﻿// https://learn.microsoft.com/en-us/visualstudio/test/using-microsoft-visualstudio-testtools-unittesting-members-in-unit-tests?view=vs-2022
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IonModTest {}
